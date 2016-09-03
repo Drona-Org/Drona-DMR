5799c017ce1ecd776501b53221e7f010f0f84561
Ankush Desai <ankushdesai@gmail.com>
2016-09-02 23:41:37 -0700

    Updated the code for DAMP

--- Src/Complan/Complan/Z3OutputParser.h
+++
@@ -1,4 +0,0 @@
-float ExtractTrajectoryCostInformation();
-void ExtractTrajectoryVelocityInformation(vector< vector<int> > & );
-bool ExtractTrajectoryRobotPositionXInformation(vector< vector<int> > &);
-bool ExtractTrajectoryRobotPositionYInformation(vector< vector<int> > &);
