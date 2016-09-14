a8974989c726d10aa54d6febeab6e71eee99caaa
Ankush Desai <ankushdesai@gmail.com>
2016-09-13 18:08:18 -0700

    updated the code and added tester for plan generator

--- Src/Lib/WorkspaceParser/WorkspaceParser.h
+++ Src/Lib/WorkspaceParser/WorkspaceParser.h
@@ -1,11 +1,26 @@
+
+#ifndef DRONA_WS_PARSER
+#define DRONA_WS_PARSER
+
 #include "Workspace.h"
+
+#ifdef __cplusplus
+extern "C" {
+#define BOOLEAN bool
+#endif
 
 /*
 Function to parse the workspace configuration
 */
-WorkspaceInfo* ParseWorkspaceConfig(char* configurationFile);
+WorkspaceInfo* ParseWorkspaceConfig(const char* configurationFile);
 
 /*
 Free the workspace configuration
 */
 void FreeWorkspaceInfo(WorkspaceInfo* info);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
