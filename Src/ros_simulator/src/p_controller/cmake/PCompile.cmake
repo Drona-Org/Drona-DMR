cf5345634058d695a15ba1a4cbe74771a29e382f
Jianqiao Yang <jq.yang@berkeley.edu>
2016-09-15 20:54:54 -0700

    use shared library

--- Src/ros_simulator/src/p_controller/cmake/PCompile.cmake
+++ Src/ros_simulator/src/p_controller/cmake/PCompile.cmake
@@ -1,4 +1,5 @@
 function (p_compile pproject p_src)
+	set(p_compile_cmd mono ${P_COMPILER_PATH})
 	get_filename_component(p_src_basename ${p_src} NAME_WE)
 	set(generated_c_src ${p_src_basename}.c)
 	set(output_dir ${CMAKE_CURRENT_BINARY_DIR}/generated/${pproject})
@@ -6,18 +7,19 @@ function (p_compile pproject p_src)
 	add_custom_command(
 		OUTPUT ${output_c_srcs}
 	    COMMAND ${CMAKE_COMMAND} -E make_directory ${output_dir}
-		COMMAND ${P_COMPILE_CMD} /outputDir:${output_dir} ${p_src}
+		COMMAND ${p_compile_cmd} /outputDir:${output_dir} ${p_src}
 		DEPENDS ${p_src}
 		WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
 		COMMENT "Compiling P ${p_src}..."
 	)
-	set(${pproject}_GENERATED_SRCS ${output_c_srcs} PARENT_SCOPE)
 
 	if (NOT (TARGET Prt))
-		add_library(Prt STATIC IMPORTED)
-		set_target_properties(Prt PROPERTIES IMPORTED_LOCATION ${P_LIB_PATH}/libPrt_static.a)
+		add_library(Prt SHARED IMPORTED)
+		set_target_properties(Prt PROPERTIES IMPORTED_LOCATION ${P_LIB_LINK_PATH}/libPrt_shared.so)
+		add_definitions( -DPRT_PLAT_LINUXUSER ) # currently ros only supports *nix/OSX
 	endif()
 
+	set(${pproject}_GENERATED_SRCS ${output_c_srcs} PARENT_SCOPE)
 	set(${pproject}_INCLUDE_DIRS ${output_dir} ${P_LIB_INCLUDE_PATH} PARENT_SCOPE)
 	set(${pproject}_LINK_LIBS Prt PARENT_SCOPE)
 
